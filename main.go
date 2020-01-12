21f2e4789caf46f3338db72bd11dcd62c9e39a15
greenpipig <1105263198@qq.com>
2020-01-12 14:09:31 +0800

    get

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func httpFlood(w http.ResponseWriter,r *http.Request){
-	_,_=fmt.Fprintf(w,"<h1>hello world</h1>")
-	fmt.Printf("r is %+v",*r)
-	fmt.Println("访问成功")
+	//读取文件中的数据，为html文件
+	if r.Method=="GET"{
+		words,_:=ioutil.ReadFile("./http_flood.txt")
+		_,_=fmt.Fprintf(w,string(words))
+		fmt.Println(r.Method)
+		fmt.Println("访问成功")
+	}
 }
 
 func main(){
-	http.HandleFunc("/httpflood",httpFlood)
+	//定义一个方法，url为/httpflood
+	http.HandleFunc("/httpFlood",httpFlood)
 	err:=http.ListenAndServe(":9090",nil)
 	if err != nil{
 		fmt.Printf("some thing wrong %v",err)
